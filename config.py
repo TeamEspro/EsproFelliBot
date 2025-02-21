7e02b7566a20c352c334c59c4d2fa1872d9ba84c
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-02-21 13:57:49 +0530

    ***

--- config.py
+++ config.py
@@ -11,12 +11,9 @@ API_HASH = "9ba740b3c2b946c837e95852a780b7f8"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "6656608288"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/yourtoofan/CHATBOT")
+OWNER_ID = int(getenv("OWNER_ID", ""))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Team/EsproFelliBot")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-SUPPORT_GRP = "kaalCarder"
-UPDATE_CHNL = "SweetToxicUpdate"
-OWNER_USERNAME = "WerewolfDemonInfo"
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", "")
     
