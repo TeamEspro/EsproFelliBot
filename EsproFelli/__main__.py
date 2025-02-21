d6a27684e8fe7786d21e1229f94670a1f6892aeb
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-02-22 00:54:26 +0530

    Update __main__.py

--- EsproFelli/__main__.py
+++ EsproFelli/__main__.py
@@ -1,3 +1,22 @@
+from pyrogram import Client, errors
+from pyrogram.enums import ChatMemberStatus, ParseMode
+
+import config
+class app(Client):
+    def __init__(self):
+        LOGGER(__name__).info(f"Starting Bot...")
+        super().__init__(
+            name="EsproMusic",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN,
+            in_memory=True,
+            parse_mode=ParseMode.HTML,
+            max_concurrent_transmissions=7,
+        )
+
+
+
 # EsproFelli/main.py
 
 # Absolute Import ka use karein
