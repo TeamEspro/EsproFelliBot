d057bcba4d4a1afb3062addc3cbb064960b9c3e5
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-02-22 01:56:02 +0530

    Update __main__.py

--- EsproFelli/__main__.py
+++ EsproFelli/__main__.py
@@ -1,4 +1,4 @@
-
+from EsproFelli.__init__ import app
 
 # EsproFelli/main.py
 
@@ -9,7 +9,7 @@ from modules.wel import welcome_message
 from modules.tagall import tag_all
 
 # Functions ko call karein
-
+app = app()
 print(felli_function())
 print(welcome_message())
 print(tag_all())
