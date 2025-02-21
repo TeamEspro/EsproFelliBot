aeeee832fe601337d2ab3f8fd15dc6584ef66811
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-02-22 01:10:08 +0530

    Update __init__.py

--- EsproFelli/__init__.py
+++ EsproFelli/__init__.py
@@ -10,7 +10,7 @@ import config
 class Loy(Client):
     def __init__(self):
         super().__init__(
-            name="Demonchat",
+            name="Loy",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
