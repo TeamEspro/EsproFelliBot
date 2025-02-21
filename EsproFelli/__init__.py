8439b8c1edab265a7eb84c1ba7f21a5564f286ff
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-02-22 01:09:38 +0530

    Update __init__.py

--- EsproFelli/__init__.py
+++ EsproFelli/__init__.py
@@ -1,5 +1,24 @@
 # EsproFelli/__init__.py
+from pyrogram import Client, errors
+from pyrogram.enums import ChatMemberStatus, ParseMode
 
+import config
+
+
+
+
+class Loy(Client):
+    def __init__(self):
+        super().__init__(
+            name="Demonchat",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN,
+            in_memory=True,
+            parse_mode=ParseMode.DEFAULT,
+        )
+
+app = Loy()
 
 from .modules.felli import felli_function
 from .modules.wel import welcome_message
