3c395f4c0d76f5af0f7553dedbd36f704195e765
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-02-22 00:43:02 +0530

    Update __init__.py

--- EsproFelli/__init__.py
+++ EsproFelli/__init__.py
@@ -1,6 +1,6 @@
 # EsproFelli/__init__.py
 
-from .modules.cot import cot_function
+
 from .modules.felli import felli_function
 from .modules.wel import welcome_message
 from .modules.tagall import tag_all
