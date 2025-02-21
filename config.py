0eff57040b4992d5dca35554cbe7f6579e6d6d11
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-02-22 01:28:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "9ba740b3c2b946c837e95852a780b7f8"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", ""))
+OWNER_ID = int(getenv("OWNER_ID", "6693611573"))
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Team/EsproFelliBot")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 SUPPORT_GROUP = "EsproSupport"
@@ -18,4 +18,4 @@ UPDATE_CHANNEL = "EsproUpdate"
 BOT_USERNAME = "FelliChatBot"
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", "")
-BOT_IMAGE = getenv("BOT_IMAGE", "")
+BOT_IMAGE = getenv("BOT_IMAGE", "https://telegra.ph/file/f1aca953494a6c4a4ad87.jpg")
