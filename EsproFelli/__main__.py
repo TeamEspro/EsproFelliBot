0569690fb57912d67df4a8f3b2dc68ee0c39bd20
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-02-22 03:03:28 +0530

    Update __main__.py

--- EsproFelli/__main__.py
+++ EsproFelli/__main__.py
@@ -1,29 +1,4 @@
 from EsproFelli.__init__ import app
-
-import asyncio
-
-# Telegram Bot API Library
-from pyrogram import Client
-
-# Bot Configuration
-
-
-# Function to send message on start
-async def send_start_message():
-    await bot.send_message(chat_id=OWNER_ID, text="✅ Bot Started Successfully!")
-
-# Bot Start Event
-@app.on_message()
-async def start_bot(_, message):
-    print("Bot is running...")
-    
-# Running the bot
-async def main():
-    async with app:
-        await send_start_message()  # Send start message to owner
-
-app.run(main())
-
 # EsproFelli/main.py
 
 # Absolute Import ka use karein
