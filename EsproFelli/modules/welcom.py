bfd272e7d8920ec9be4f5d3ee413731ddf437c15
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-02-23 11:08:45 +0530

    Update welcom.py

--- EsproFelli/modules/welcom.py
+++ EsproFelli/modules/welcom.py
@@ -6,14 +6,10 @@ async def welcome(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members:
         await update.message.reply_text(f"Welcome {member.first_name} to the group! 🎉")
 
-def main():
     app = Application.builder().token(BOT_TOKEN).build()
     
     # New members ke liye handler
     app.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, welcome))
     
     print("Bot is running...")
-    app.run_polling()
-
-if __name__ == "__main__":
-    main()
+    
