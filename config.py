adac2e3d583fb6848fe508eda901245e4df09df9
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-02-23 22:51:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 class Config:
     API_ID=24509589
     API_HASH="717cf21d94c4934bcbe1eaa1ad86ae75"
-    BOT_TOKEN="7714423209:AAGdN1J4EV1rEZbK_m_YYc7AtRWomRVCdvk"
+    BOT_TOKEN="8183943331:AAFMlQ8swPHtelpJXVtgYwmJvzxHmhQdYfI"
     SUDO = list(int(i) for i in os.environ.get("SUDO", "6693611573").split(" "))
     START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
     BOT_ID=8084144216
