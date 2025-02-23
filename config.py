0ca826aebe42acec7c71e6da614c02eea1e364f0
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-02-23 22:01:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ class Config:
     SUDO = list(int(i) for i in os.environ.get("SUDO", "6693611573").split(" "))
     START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
     BOT_ID=8084144216
+    MONGO_URL = "mongodb+srv://rogip74246:fCM4fkn8jWv9zizJ@ritikraj.mylvo.mongodb.net/?retryWrites=true&w=majority&appName=ritikraj"
     BOT_USERNAME="ZuliaiBot"
     BOT_NAME="ZuliaiBot"
