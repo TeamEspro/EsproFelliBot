e40257ac88ba26dd50a509035f3802aea81f63ce
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-02-23 13:04:48 +0530

    Update __main__.py

--- EsproFelli/__main__.py
+++ EsproFelli/__main__.py
@@ -1,6 +1,35 @@
-
 from EsproFelli import app, LOG
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup , CallbackQuery 
+
+
+BOT_IMAGE = "https://telegra.ph/file/f1aca953494a6c4a4ad87.jpg"
+
+
+
+@bot.on_message(filters.command("start") & filters.private)
+async def start_(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"{BOT_IMAGE}",
+        caption=f"""**━━━━━━━━━━━━━━━━━━━━━━━━
+Hɪ I'ᴍ A Aᴅᴠᴀɴᴄᴇ 𝐇ocαηє [•ᴧғᴋ•].\n\n Nᴀᴍᴇ Is 𝐇ocαηє [•ᴧғᴋ•] Fᴏʀᴍ Iɴᴅɪᴀ \n\n ✔ I'ᴍ A Aʀᴛɪғɪᴄɪᴀʟ Iɴᴛᴇʟʟɪɢᴇɴᴄᴇ\n\n/ᴄʜᴀᴛʙᴏᴛ - [ᴏɴ/ᴏғғ] Tʜɪs Cᴏᴍᴍᴀɴᴅ Usᴇ Oɴʟʏ Aɴʏ Gʀᴏᴜᴘ
+
+💕Jᴜsᴛ Aᴅᴅ Mᴇ » Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ Aɴᴅ 
+EɴJᴏʏ Sᴜᴘᴇʀ Qᴜᴀʟɪᴛʏ Cʜᴀᴛ
+━━━━━━━━━━━━━━━━━━━━━━━━**""",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "✚ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ✚", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+                ]
+                
+           ]
+        ),
+    )
+
+
 
 if __name__ == "__main__":
     LOG.info("started")
-    
+    app.run()
