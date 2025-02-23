1ef2377d61b0fc942ccd0d3a431ee99d4e6efc5c
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-02-23 11:02:07 +0530

    Update __init__.py

--- EsproFelli/__init__.py
+++ EsproFelli/__init__.py
@@ -1,25 +1,49 @@
-# EsproFelli/__init__.py
-from pyrogram import Client, errors
-from pyrogram.enums import ChatMemberStatus, ParseMode
+import os
+import logging 
+from pyrogram import Client
+from config import Config 
 
-import config
+# enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
 
-
-
-
-class app(Client):
-    def __init__(self):
-        super().__init__(
-            name="app",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            bot_token=config.BOT_TOKEN,
-            in_memory=True,
-            parse_mode=ParseMode.DEFAULT,
-        )
+LOG = logging.getLogger(__name__)
 
 
+ENV = bool(os.environ.get("ENV",False))
 
-from EsproFelli.modules.felli import felli_function
-from EsproFelli.modules.wel import welcome_message
+if ENV:
+    API_ID=int(os.environ.get("API_ID",""))
+    API_HASH=str(os.environ.get("API_HASH",""))
+    BOT_TOKEN=str(os.environ.get("BOT_TOKEN",""))
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "6691393517").split(" "))
+    START_IMG=str(os.environ.get("START_IMG",""))
+    BOT_ID=int(os.environ.get("BOT_ID",""))
+    BOT_USERNAME=str(os.environ.get("BOT_USERNAME",""))
+    BOT_NAME=str(os.environ.get("BOT_NAME",""))
+
+else:
+    API_ID=Config.API_ID
+    API_HASH=Config.API_HASH
+    BOT_TOKEN=Config.BOT_TOKEN
+    SUDO=Config.SUDO
+    START_IMG=Config.START_IMG
+    BOT_ID=Config.BOT_ID
+    BOT_USERNAME=Config.BOT_USERNAME
+    BOT_NAME=Config.BOT_NAME
+
+
+
+app=Client(
+    "BOT",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="EsproFelli.modules")
+     )
+
+LOG.info("starting the bot....")
 
