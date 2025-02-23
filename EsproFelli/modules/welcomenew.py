d8f57af53210267d5e9edee150bd5e34d8739b69
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-02-23 10:35:55 +0530

    Update welcomenew.py

--- EsproFelli/modules/welcomenew.py
+++ EsproFelli/modules/welcomenew.py
@@ -1,14 +1,13 @@
 from telegram import Update
 from telegram.ext import Application, MessageHandler, filters, CommandHandler, CallbackContext
-
-TOKEN = "7734465358:AAGMuYNK4DSMq8i7W5HofNa6WHr6xtbgRIU"
+from EsproFelli import app
 
 async def welcome(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members:
         await update.message.reply_text(f"Welcome {member.first_name} to the group! 🎉")
 
 def main():
-    app = Application.builder().token(TOKEN).build()
+    app = Application.builder().token(BOT_TOKEN).build()
     
     # New members ke liye handler
     app.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, welcome))
