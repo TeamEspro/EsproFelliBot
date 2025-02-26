ee0b97749bf38dd69752209bfccbaf1c3d6acc04
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-02-26 21:40:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,4 @@ class Config:
     BOT_TOKEN="7500257786:AAF-edacsdK0xQkF0rZwS6CRIPUCmEyuDR8"
     SUDO = list(int(i) for i in os.environ.get("SUDO", "6693611573").split(" "))
     START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
-    BOT_ID=8084144216
-    MONGO_URL = "mongodb+srv://rogip74246:fCM4fkn8jWv9zizJ@ritikraj.mylvo.mongodb.net/?retryWrites=true&w=majority&appName=ritikraj"
-    BOT_USERNAME="ZuliaiBot"
-    BOT_NAME="ZuliaiBot"
+    BOT_ID=7500257786
