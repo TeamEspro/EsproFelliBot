1439b75b4b0030daeb9e7f983530ef98b7e9cf39
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-02-27 00:33:25 +0530

    Update __init__.py

--- EsproFelli/__init__.py
+++ EsproFelli/__init__.py
@@ -3,48 +3,47 @@ import logging
 from pyrogram import Client
 from config import Config
 
-# Enable logging
+enable logging
+
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
-    level=logging.INFO,
+format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+level=logging.INFO,
 )
 
-LOG = logging.getLogger(__name__)
+LOG = logging.getLogger(name)
 
-ENV = bool(os.environ.get("ENV", False))
+ENV = bool(os.environ.get("ENV",False))
 
 if ENV:
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = str(os.environ.get("API_HASH", ""))
-    BOT_TOKEN = str(os.environ.get("BOT_TOKEN", ""))
-    MONGO_URL = str(os.environ.get("MONGO_URL", ""))
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "6693611573").split(" "))
-    START_IMG = str(os.environ.get("START_IMG", ""))
-    BOT_ID = int(os.environ.get("BOT_ID", ""))
-    BOT_USERNAME = str(os.environ.get("BOT_USERNAME", ""))
-    BOT_NAME = str(os.environ.get("BOT_NAME", ""))
-else:
-    API_ID = Config.API_ID
-    API_HASH = Config.API_HASH
-    BOT_TOKEN = Config.BOT_TOKEN
-    MONGO_URL = Config.MONGO_URL
-    SUDO = Config.SUDO
-    START_IMG = Config.START_IMG
-    BOT_ID = Config.BOT_ID
-    BOT_USERNAME = Config.BOT_USERNAME
-    BOT_NAME = Config.BOT_NAME
+API_ID=int(os.environ.get("API_ID",""))
+API_HASH=str(os.environ.get("API_HASH",""))
+BOT_TOKEN=str(os.environ.get("BOT_TOKEN",""))
+MONGO_URL =str(os.environ.get("MONGO_URL", ""))
+SUDO = list(int(i) for i in os.environ.get("SUDO", "6693611573").split(" "))
+START_IMG=str(os.environ.get("START_IMG",""))
+BOT_ID=int(os.environ.get("BOT_ID",""))
+BOT_USERNAME=str(os.environ.get("BOT_USERNAME",""))
+BOT_NAME=str(os.environ.get("BOT_NAME",""))
 
-# Initialize bot with multiple plugin folders
-app = Client(
-    "BOT",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root=["EsproFelli.modules", "EsproFelli.tools"])  # Multiple folders
+else:
+API_ID=Config.API_ID
+API_HASH=Config.API_HASH
+BOT_TOKEN=Config.BOT_TOKEN
+MONGO_URL=Config.MONGO_URL
+SUDO=Config.SUDO
+START_IMG=Config.START_IMG
+BOT_ID=Config.BOT_ID
+BOT_USERNAME=Config.BOT_USERNAME
+BOT_NAME=Config.BOT_NAME
+
+app=Client(
+"BOT",
+api_id=API_ID,
+api_hash=API_HASH,
+bot_token=BOT_TOKEN,
+plugins=dict(root=["EsproFelli.modules","EsproFelli.tools"])
 )
 
-LOG.info("Starting the bot....")
+LOG.info("starting the bot....")
 
-if __name__ == "__main__":
-    app.run()
