ebc3070c650261c32d8435c8906e4196bdb25c6a
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-02-27 00:14:31 +0530

    Update __init__.py

--- EsproFelli/__init__.py
+++ EsproFelli/__init__.py
@@ -46,6 +46,13 @@ app=Client(
     bot_token=BOT_TOKEN,
     plugins=dict(root="EsproFelli.modules")
      )
+app=Client(
+    "BOT",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="EsproFelli.tools")
+)
 
 LOG.info("starting the bot....")
 
